fa31887b48e283b4c304894479eda8910acf7025
KevinBarrera <kev97barrera@gmail.com>
2019-03-13 17:01:27 -0600

    3 de 4, ya casi jeje

--- src/App.js
+++ src/App.js
@@ -2,23 +2,55 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class Oracion extends Component {
-  render(){
-    return(
-      <div className="frases">
-        {this.props.texto}
-      </div>
-    );
-  }
-}
-
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      arriba:'Ora ora...',
+      abajo:'que se atora!',
+    }
+    this.handleFraseTop = this.handleFraseTop.bind(this);
+    this.handleFraseBottom = this.handleFraseBottom.bind(this);
+  }
+
+  handleFraseTop(e){
+    this.setState({
+      arriba: e.target.value
+    });
+  }
+
+  handleFraseBottom(e){
+    this.setState({
+      abajo: e.target.value
+    });
+  }
+
   render() {
     return (
       <div className="App">
+        <div className="formulario">
+          <form>
+            <input
+              type="text"
+              name="arriba"
+              value={this.state.arriba}
+              onChange = {this.handleFraseTop} 
+            />
+            <input
+              type="text"
+              name="abajo"
+              value={this.state.abajo}
+              onChange = {this.handleFraseBottom}  
+            />
+          </form>
+        </div>
         <div className="DogeMeme">
-          <Oracion texto="Hola hola"></Oracion>
-          <Oracion texto="que se atora"></Oracion>
+          <div className="frases">
+            {this.state.arriba}
+          </div>
+          <div className="frases">
+            {this.state.abajo}
+          </div>
         </div>
       </div>
     );
